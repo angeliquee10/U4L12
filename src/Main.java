ac9ea5fcfb9f3aae6a3c0309d8c469c4cebd6736
Angelique Escamilla <angeliquee10@nycstudents.net>
2026-03-12 08:41:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle r = new Rectangle(20, 10, 10, 15);
+
+        ArrayList<Rectangle> rectangles = new ArrayList<>();
+        for(int i = 0; i < 1000000; i++)
+        {
+            //make randomiing scripts
+            int len = 0;
+            int wid = 0;
+            int xCoord = 0;
+            int yCoord = 0;
+
+
+        }
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+
+    private int length;
+    private int width;
+    private int xCoordinate;    //top left
+    private int yCoordinate;    //top left
+
+    public Rectangle(int len, int wid, int x, int y)
+    {
+        length = len;
+        width = wid;
+        xCoordinate = x;
+        yCoordinate = y;
+    }
+
+
+}
